bb0bb6ddd0a52346121c8f72c498eb28b360bc07
ritztyama <96643107+ritztyama@users.noreply.github.com>
2024-01-03 16:25:56 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ritztyama.github.io',
-  base: '/',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
